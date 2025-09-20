221eeb4090d529724ce3118d4a8fd7a8525c8269
minaripenguin <minaripenguin@users.noreply.github.com>
2025-09-20 09:04:36 +0000

    config: Load boot image profile from frameworks/base
    [YAAP:] art boot image profile is only intended for thin art builds
    without frameworks/base.
    
    Change-Id: I60f3117c6ba948311af9f3fe048d0cf304fc283a

--- config/alch3my.mk
+++ config/alch3my.mk
@@ -56,6 +56,10 @@ endif
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-alch3my-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-alch3my-product.xml
 
+# Use a generic profile based boot image by default
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
+
 # Private Keys
 ifneq ($(filter OFFICIAL Official official,$(ALCH3MY_BUILD_TYPE)),)
 include vendor/lineage-priv/keys/keys.mk
