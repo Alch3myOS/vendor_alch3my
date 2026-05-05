d1eddf90ce61fdb4881e2f80ab4b2879df1e64e8
Ghosuto <clash.raja10@gmail.com>
2026-05-06 00:31:08 +0900

    config: dexpreopt gamespace

--- config/common.mk
+++ config/common.mk
@@ -277,8 +277,8 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
 endif
 endif
 
-# SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += \
+    GameSpace \
     Launcher3QuickStep \
     NexusLauncherRelease \
     Settings \
