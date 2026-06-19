4bbecc2fb9eb5b99c3d5420da945477a742ca99e
Pranav Vashi <neobuddy89@gmail.com>
2026-06-20 02:11:08 +0530

    config: Drop memtag props
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- config/alch3my.mk
+++ config/alch3my.mk
@@ -54,13 +54,6 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/priv-app/OmniStyle/OmniStyle.apk \
     system/priv-app/MatLog/MatLog.apk
 
-# Disable async MTE on a few processes
-PRODUCT_SYSTEM_EXT_PROPERTIES += \
-    persist.arm64.memtag.app.com.android.se=off \
-    persist.arm64.memtag.app.com.google.android.bluetooth=off \
-    persist.arm64.memtag.app.com.android.nfc=off \
-    persist.arm64.memtag.process.system_server=off
-
 # Enable Material Design 3 Expressive
 PRODUCT_PRODUCT_PROPERTIES += is_expressive_design_enabled=true
 
