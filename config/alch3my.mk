9ce61f26a4cb8de0c12cb1a094df8a98e1b8ab05
Ghosuto <clash.raja10@gmail.com>
2026-07-02 14:44:18 +0000

    config: Hide advance display color [2/2]
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- config/alch3my.mk
+++ config/alch3my.mk
@@ -157,11 +157,13 @@ BYPASS_CHARGE_SUPPORTED := true
 HBM_SUPPORTED := true
 HBM_NODE := /sys/class/backlight/panel0-backlight/hbm_mode
 USE_REALITY_ENGINE := true
+USE_ADVANCED_DISPLAY_COLOR := false
 
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.battery_bypass_supported=$(BYPASS_CHARGE_SUPPORTED) \
     persist.sys.hbmservice_file=$(HBM_NODE) \
     persist.display.reality.engine.enabled=$(USE_REALITY_ENGINE) \
+    persist.display.color.advanced.enabled=$(USE_ADVANCED_DISPLAY_COLOR) \
     persist.sys.udfps.custom=$(TARGET_CUSTOM_UDFPS)
 
 # Quick Switch
